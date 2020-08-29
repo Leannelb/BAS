67639cca09788c066376643e6c0369378dc38735
leannelb <leannelaceybyrne@outlook.co>
2020-08-29 21:28:36 +0100

    set up product entity with getter and setter

---
+++ API/Entities/Product.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    public class Product
+    {
+        //type prop and enter and the below line is wrote for you
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
